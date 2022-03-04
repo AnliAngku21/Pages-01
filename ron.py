94c087113fec6e6798305a4536b37e62f2e30007
Anli Angku <100510435+AnliAngku21@users.noreply.github.com>
2022-03-04 10:39:51 +0700

    Create ron.py

---
+++ ron.py
@@ -0,0 +1,28 @@
+--BEGIN SOURCEDEFENDER FILE---
+GhUVMY*aCCH]!<U/Xs\n?.[j33N*0@
+GhN0W!m(=b&?I,W78eAO7$'\CNhrT9
+\rCjT'rJ"oUlM:e5HsqkEtE[O?^P`u
+-mJYPi:te%"f?WH#5I\tH.$Ujc"7Nb
+lW+/P+3,Y7/Kieke%r3j@o#AK?`0Ys
+>F01NB+-iS'%P\lE]\+Oe\?_m9W]9=
+n6SnrF$$+lC74o76PIYrP4<_neN4GQ
+"J_h^Bbm\(;hLA[WJbaSGXT1@TD2uV
+C1Jqt"4Dp\2<,oTcoJ"f=7?hea<"9P
+n6J(D<jWn%E"j+%j=b[!IMdf&4K7'\
+htDmq-0-`Iq6DW]P^>P3gXNKEdq)']
+6`Zc`dIJfbbW*Eb_0MHu@YFoCq0VRc
+n\'jp'SG)&[c9;;SEESkM&ZpFN"WJ?
+r'O,L$mC;B#^ER*PVfCOKb/;[:fTY2
+@1#\C]mNON2*f4/J@SQ7nuBO1,GlAp
+eMQWB7(fnN"`[QF%K<A:O,V<BlGB/A
+?6qS[0l(9g&QmKHTGH1$@==,oM)(D)
+@GgsI(bVJah3PN2nB#9LFfA9E[rS/W
+97;o_4o3/L&.3"rs2Jj"D6l8)mhlEj
+CcMSqMn#R!]59_rdGUVl&%+2'&3Q;P
+iadZ<f!$m26nI[I#J1<HC)tF:@44[L
+3Q[mS1_[Y#]AJI1HKlf6Ks)sdh%iCk
++-#WT`5JmJH.^?8qH_lLp0urU[Fk(1
+VR"(sr,m25],$/MUYM[*l3?!bi6>um
+"FWm#b^^0^8+oG-+rf*+B*f)!Tje+9
+("!!]
+---END SOURCEDEFENDER FILE----
