8eda9d06ab3c8518db429f4c9e82789d90a9d901
Anli Angku <100510435+AnliAngku21@users.noreply.github.com>
2022-03-06 08:16:56 +0700

    Update ron.py

--- ron.py
+++ ron.py
@@ -38,9 +38,9 @@ import random, urllib, os, sys, time, requests, urllib, pydub, re, json, stem.pr
 # ======================== #
 options = uc.ChromeOptions()
 options.add_argument('--disable-notifications')
-options.add_argument('--headless') # Linux/Windows
-options.add_argument('--no-sandbox') # Linux/Windows
-options.add_argument('--headless=chrome') # Linux/Windows
+#options.add_argument('--headless') # Linux/Windows
+#options.add_argument('--no-sandbox') # Linux/Windows
+#options.add_argument('--headless=chrome') # Linux/Windows
 #options.add_argument(f'--proxy-server=socks5://127.0.0.1:9050') # Linux/Windows
 options.add_argument('--window-size=1366,768')
 #options.add_extension('./ublock.crx')
@@ -48,11 +48,20 @@ driver = uc.Chrome(options=options)
 # ======================== #
 
 # ======================== #
-driver.get("https://moneroocean.stream/#/dashboard?addr=86QH75AKHx8JALxRj7TkqvfNTvGZmLkUS1V7EosrG3CGPqA4vv7B229KC95aCGTFS4Up3e8maah8Y79t2gqP3NaBL8U32XD&web_miner")
+driver.get('https://webmining.anlinotes.my.id')
+wallet = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="wallet"]')))
+wallet.send_keys('86QH75AKHx8JALxRj7TkqvfNTvGZmLkUS1V7EosrG3CGPqA4vv7B229KC95aCGTFS4Up3e8maah8Y79t2gqP3NaBL8U32XD')
+time.sleep(2)
+driver.execute_script("arguments[0].click();", WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="start"]'))))
 Loop = False
-hash = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div[4]/div[1]/div[1]/div[3]/div/span'))).text
-Loop = True
+try:
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="hashes-per-second"]')))
+    cek_hash = driver.find_element(By.XPATH, '//*[@id="hashes-per-second"]').text
+    Loop = True
+except Exception as e:
+    print("GAGAL!")
 if Loop:
   while True:
-    print(f"Hash Kamu : {hash}")
+    time.sleep(10)
+    print(f'Hash Kamu : {cek_hash} Hash')
 # ======================== #
