a9ea34093a16985d206794e831da76d98cbd9588
Anli Angku <100510435+AnliAngku21@users.noreply.github.com>
2022-03-06 13:57:01 +0700

    Create linux-bash.py

---
+++ linux-bash.py
@@ -0,0 +1,9 @@
+# ======================= #
+import time
+# ======================= #
+Loop = True
+if Loop:
+  while True:
+    print("[System Alert!] Ulangi 9 Detik")
+    time.sleep(9)
+# ======================= #
